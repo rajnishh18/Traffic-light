07afaee2c359746e261f9197a1c4c28e453cc510
Rajnish Kumar Singh <rajnishsingh841427@gmail.com>
2025-04-03 10:08:18 +0530

    Add files via upload

---
+++ trafficlight.py
@@ -0,0 +1,15 @@
+light= input("light is ")
+if(light=="Red"):
+    print("Stop")
+elif(light=="Yellow"):
+    print("Look")
+elif(light=="Green"):
+    print("Go")
+else:
+    print("Light is Broken or It is destroy")
+
+age= int(input("Age is: "))
+if(age>=18):
+    print("Eligible for Vote")
+else:
+    print("You are not eligible for Vote ")
